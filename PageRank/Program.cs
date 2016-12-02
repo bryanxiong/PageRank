0576491f1a32737321fb176f8f811774b6d4e181
bryanxiong <bxiong17@my.whitworth.edu>
2016-12-02 13:52:56 -0800

    Grrr

--- PageRank/Program.cs
+++ PageRank/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PageRank
 {
-    class Program
+    public class Program
     {
         public int[,] path = new int[10, 10];
         public double[] pagerank = new double[10];
@@ -16,56 +16,61 @@ namespace PageRank
             double init, c = 0;
             int u = 1, k = 1;
             double[] temp = new double[10];
+
             init = 1 / n;
-            System.Console.WriteLine("N value:" + n + "\t init value: " + init + "\n");
+
+            System.Console.WriteLine("\nN value:" + n + "\t init value: " + init + "\n");
 
             for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine("Init is: " + init);
+            //{
+                //Console.WriteLine("Init is: " + init);
                 this.pagerank[i] = init;
-                Console.WriteLine(this.pagerank[i] + " ");
+               // Console.WriteLine(this.pagerank[i] + " ");
 
-            }
-            System.Console.WriteLine("Inital PageRank Values, 0th step");
+            //}
+            System.Console.WriteLine("\nInital PageRank Values, 0th step\n");
             for (int j = 1; j <= n; j++)
             {
-                System.Console.WriteLine("Page Rank of " + j + " is: \t " + this.pagerank[j]);
+                System.Console.WriteLine("Page Rank of " + j + " is: \t " + this.pagerank[j-1]);
             }
 
             while (u <= 2)
             {
+                
                 for (int i = 0; i < n; i++)
                 {
-                    Console.WriteLine("Temp is: " + temp[i]);
                     temp[i] = this.pagerank[i];
-                    Console.WriteLine("Temp is now: " + temp[i]);
-                    Console.WriteLine("Page rank is: "+ this.pagerank[i]);
                     this.pagerank[i] = 0;
                 }
+
                 for (int j = 0; j < n; j++)
-                {
+                //{ 
                     for (int h = 0; h < n; h++)
                     {
                         if (path[h, j] == 1)
-                        {
+                        //{
+                        { 
                             k = 1;
                             c = 0;
+                        //}
 
                             while (k <= n)
                             {
-                                if (path[h, k] == 1)
-                                {
-                                    c = c + 1;
+                            if (path[h, k] == 1)
+                            //{
+                                c = c + 1;
+                           // }
                                     k = k + 1;
-                                }
+                                //}
                             }
-                            this.pagerank[h] = this.pagerank[h] + temp[j] * (1 / c);
-                        }
-
+                        this.pagerank[j] = this.pagerank[j] + temp[h] * (1 / c);
                     }
-                    System.Console.WriteLine("Page Rank of " + j + " is: \t" + this.pagerank[j]);
-                    u = u + 1;
+                    
                 }
+                System.Console.WriteLine("\nAfter " + u + "th Step\n");
+                for (int i = 1; i <= n; i++)
+                    System.Console.WriteLine("Page Rank of " + i + " is: \t" + this.pagerank[i-1]);
+                u = u + 1;
             }
         }
         static void Main(string[] args)
@@ -92,9 +97,8 @@ namespace PageRank
                 }
                 p.calc(nodes);
             }
-
+            Console.ReadKey();
         }
     }
 }
-
-//http://codispatch.blogspot.com/2015/12/java-program-implement-google-page-rank-algorithm.html
+//\http://codispatch.blogspot.com/2015/12/java-program-implement-google-page-rank-algorithm.html
